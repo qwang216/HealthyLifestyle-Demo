98fdd1350109dc3b88a5ca61d01074dbb89d65b4
Jason Wang <qwang216@gmail.com>
2021-06-12 15:52:57 -0400

    Initial Commit

---
+++ Healthy Lifestyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Healthy Lifestyle
+//
+//  Created by Jason wang on 6/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
